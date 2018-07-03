b0d8912725a16caaff3c0559ac257e3b381ce5e4
Yuri Furusho <yuri.furusho.positivo@gmail.com>
2018-07-03 17:16:47 -0300

    9. NotesAdapter Conversão automatica

---
+++ app/src/main/java/com/udacity/notepad/recycler/NotesAdapter.kt
@@ -0,0 +1,69 @@
+package com.udacity.notepad.recycler
+
+import android.content.Context
+import android.os.Handler
+import android.os.Looper
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+import com.udacity.notepad.R
+import com.udacity.notepad.data.DataStore
+import com.udacity.notepad.data.Note
+
+import java.util.ArrayList
+
+class NotesAdapter(private val context: Context) : RecyclerView.Adapter<NotesAdapter.NotesViewHolder>() {
+    private var notes: List<Note> = ArrayList()
+    private var isRefreshing = false
+
+    init {
+        setHasStableIds(true)
+    }
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        refresh()
+    }
+
+    override fun getItemId(position: Int): Long {
+        return notes[position].id.toLong()
+    }
+
+    override fun getItemCount(): Int {
+        return notes.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotesViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_note, parent, false)
+        return NotesViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: NotesViewHolder, position: Int) {
+        val note = notes[position]
+        holder.text.text = note.text
+    }
+
+    fun refresh() {
+        if (isRefreshing) return
+        isRefreshing = true
+        DataStore.execute {
+            val notes = DataStore.getNotes().all
+            Handler(Looper.getMainLooper()).post {
+                this@NotesAdapter.notes = notes
+                notifyDataSetChanged()
+                isRefreshing = false
+            }
+        }
+    }
+
+    class NotesViewHolder internal constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        internal var text: TextView
+
+        init {
+            text = itemView.findViewById(R.id.text)
+        }
+    }
+}
